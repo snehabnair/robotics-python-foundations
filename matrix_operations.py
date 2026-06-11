aed179c4d782ad4155470cd3b3758752759e61dc
snehabnair <114433286+snehabnair@users.noreply.github.com>
2026-06-12 00:55:20 +0530

    Add matrix operations for addition and multiplication

---
+++ matrix_operations.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+A = np.array([[1, 2], [3, 4]])
+B = np.array([[5, 6], [7, 8]])
+
+print("Matrix A:")
+print(A)
+
+print("Matrix B:")
+print(B)
+
+print("Addition:")
+print(A + B)
+
+print("Multiplication:")
+print(np.dot(A, B))
